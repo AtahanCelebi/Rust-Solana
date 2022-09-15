9162a6a068c16a53feaf9493d7b03afb8afe098b
atahan <39523757+AtahanCelebi@users.noreply.github.com>
2022-09-15 20:39:00 +0300

    calculate-areaOf-rectangle

---
+++ isEven-Funct/main2.rs
@@ -0,0 +1,11 @@
+fn main() {
+    
+    let width1:u32 = 30;
+    let height1:u32 = 50;
+
+    println!("The area f rectangle is {}",area(width1,height1));
+}
+
+fn area(width:u32,height:u32) -> {
+width*height  //returns
+}
